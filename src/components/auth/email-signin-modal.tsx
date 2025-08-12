1606529fc47098905f8ca8cfe5843b019388994e
hellolucient <hellolucient@gmail.com>
2025-08-12 20:01:47 +0700

    fix: import DialogDescription for SSR build

--- src/components/auth/email-signin-modal.tsx
+++ src/components/auth/email-signin-modal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
